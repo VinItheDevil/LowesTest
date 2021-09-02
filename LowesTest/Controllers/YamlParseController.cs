347a78810d0df43d28d93ac4668fde321e0c67ae
VinItheDevil <88677396+VinItheDevil@users.noreply.github.com>
2021-09-02 11:54:00 +0530

    Lowes Test v1

--- LowesTest/Controllers/YamlParseController.cs
+++ LowesTest/Controllers/YamlParseController.cs
@@ -11,39 +11,67 @@ using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using System.Diagnostics;
+using System.Threading;
+
 namespace LowesTest.Controllers
 {
     public class YamlParseController : ApiController
     {
+        public class MyClass
+        {
+            public Dictionary<string, List<string>> Color { get; set; }
+        }
         public HttpResponseMessage Post()
         {
             HttpResponseMessage result = null;
             var httpRequest = HttpContext.Current.Request;
-            Dictionary<string, int> countDic= new Dictionary<string, int>();
-            if (httpRequest.Files.Count > 0)
+            Dictionary<string, int> countDic = new Dictionary<string, int>();
+            int rCount = 0, bCount = 0;
+            if (httpRequest.Files.Count > 0 && httpRequest.Files.Count <= 100)
             {
-                foreach (string file in httpRequest.Files)
+                const int NumberOfRetries = 3;
+                const int DelayOnRetry = 1000;
+                for (int i = 1; i <= NumberOfRetries; ++i)
                 {
-                    var postedFile = httpRequest.Files[file];
-                    var filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + postedFile.FileName);
-                    System.IO.Directory.CreateDirectory(filePath);
-                    postedFile.SaveAs(Path.Combine(filePath, postedFile.FileName));
-                    var deserializer = new DeserializerBuilder()
-                .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                .Build();
-                    var yamlObject = (List<Object>)deserializer.Deserialize(new StreamReader(filePath));
-                    int rCount = 0, bCount = 0;
-                    foreach (var s in yamlObject)
+                    try
                     {
-                        if (s.ToString() == "red")
-                            rCount++;
-                        else if (s.ToString() == "blue")
-                            bCount++;
+                        foreach (string file in httpRequest.Files)
+                        {
+                            var postedFile = httpRequest.Files[file];
+                            var filePath = HttpContext.Current.Server.MapPath("~/App_Data/");
+                            filePath = Path.Combine(filePath, postedFile.FileName);
+                            postedFile.SaveAs(filePath);
+                            var deserializer = new DeserializerBuilder()
+                        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                        .Build();
+                            var streamRead = new StreamReader(filePath);
+                            var yamlObject = deserializer.Deserialize<Dictionary<string, List<string>>>(streamRead);
+
+                           // Debug.WriteLine(yamlObject.ToString());
+                            if (yamlObject.Keys.Where(x => x.Equals("color")).Any())
+                            {
+                                var colorList = yamlObject.Values.ToList();
+                                foreach (var s in colorList[0])
+                                {
+                                    if (s.Equals("red"))
+                                        rCount++;
+                                    else if (s.Equals("blue"))
+                                        bCount++;
+                                }
+                            }
+                            streamRead.Close();
+                        }
+
+                        break; 
                     }
-                    countDic.Add("Red Count", rCount);
-                    countDic.Add("Blue Count", bCount);
-                    //Debug.WriteLine(rCount + " " + bCount);
-                }
+                    catch (IOException e) when (i <= NumberOfRetries)
+                    {
+        
+                        Thread.Sleep(DelayOnRetry);
+                    }
+                }              
+                countDic.Add("Red Count", rCount);
+                countDic.Add("Blue Count", bCount);
                 result = Request.CreateResponse(HttpStatusCode.OK, countDic);
             }
             else
